310117abb7600bc0b30eef928ae4e2ed4d296b42
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-08-09 22:12:53 -0300

    Aprendendo componentes e propriedades

---
+++ meu-app/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Header from './header.js';
+
+function App() {
+  return (
+    <Header nome="ColdDev" links = {["Sobre", "Comprar", "Contato"]}></Header>  
+  );
+}
+
+export default App;
