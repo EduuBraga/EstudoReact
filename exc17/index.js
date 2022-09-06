3c562a4ec15e9965f7bbc5d78e90e2985d09f4c1
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-09-06 00:12:58 -0300

    Aprendi a passar a carga para a Action

--- exc17/index.js
+++ exc17/index.js
@@ -1,13 +1,13 @@
 const redux = require('redux')
 const createStore = redux.createStore
 
-const incrementAction = { type: "INCREMENT" }
+const incrementAction = (value)=>{ return{ type: "INCREMENT", payload: value || 1}}
 const decrementAction = { type: "DECREMENT" }
 
 function counterReducer(state = 0, action) {
   switch (action.type) {
     case "INCREMENT":
-      return state + 1
+      return state + action.payload
     case "DECREMENT":
       return state - 1
     default:
@@ -19,8 +19,6 @@ const store = createStore(counterReducer)
 console.log(store.getState())
 store.subscribe(()=>{console.log(store.getState())})
 
-store.dispatch(incrementAction)
-store.dispatch(incrementAction)
-store.dispatch(decrementAction)
-store.dispatch(incrementAction)
-store.dispatch(incrementAction)
+store.dispatch(incrementAction())
+store.dispatch(incrementAction(5))
+store.dispatch(incrementAction(5))
