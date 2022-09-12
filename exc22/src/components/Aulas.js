10beafe58c92a4d6cc5009f0f1d32a50a3c70d1f
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-09-12 18:12:54 -0300

    ReactRouter | UseMatch

--- exc22/src/components/Aulas.js
+++ exc22/src/components/Aulas.js
@@ -1,8 +1,16 @@
 import { data } from "./Data";
 import { Link, useMatch } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export function Aulas() {
   const match = useMatch("/aulas")
+
+  const [url, setUrl] = useState('')
+
+  useEffect(()=>{
+    const newUrl = match.pathname.slice(-1) === '/' ? match.pathname : match.pathname + '/' 
+    setUrl(newUrl)
+  }, [])
 
   return (
     <div className="aulas">
@@ -11,7 +19,7 @@ export function Aulas() {
         {data.map((aula) =>
           <div key={aula.id} className="item__aulas">
             <li key={aula.id}>{aula.tittle} -</li>
-            <Link to={`${match.pathname}/${aula.id}`}>Aula {aula.id}</Link>
+            <Link to={`${url}${aula.id}`}>Aula {aula.id}</Link>
           </div>
         )}
       </ul>
